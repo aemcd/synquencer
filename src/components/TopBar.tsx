b42d8deb1b3cb8a4fe24c717b1c9cebf924c8bb5
Emery <mcdonnea@purdue.edu>
2023-02-22 02:54:50 -0500

    Added BPM, time signature, and grid resolution to top bar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -3,6 +3,12 @@ export default function TopBar() {
         <div className="top-bar">
             <button className="top-button">▶</button>
             <button className="top-button">◼</button>
+            <div className="settings">
+                <input className="settings-input" defaultValue="120" style={{width: "36px"}} maxLength={3}></input> BPM |
+                <input className="settings-input" defaultValue="4/4" style={{width: "56px"}}></input>|
+                <input className="settings-input" defaultValue="1/16" style={{width: "64px"}}></input>
+            </div>
+            
         </div>
     )
 }

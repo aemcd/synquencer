d0e2761e456067825edc8dd421113d12758b0360
aemcd <70421616+aemcd@users.noreply.github.com>
2023-02-26 18:07:24 -0500

    Sequencer canvas created

---
+++ src/components/PianoRoll.tsx
@@ -0,0 +1,7 @@
+export default function PianoRoll() {
+    return(
+        <div>
+            <canvas id="roll" width="512" height="384" style={{border:"1px solid"}} />
+        </div>
+    )
+}
--- src/pages/sequencer/index.tsx
+++ src/pages/sequencer/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import TopBar from '@/components/TopBar'
+import PianoRoll from '@/components/PianoRoll'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <TopBar />
+      <PianoRoll />
     </>
   )
 }

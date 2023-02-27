28b4992f6f61e271410ec98b0ab0d78c1747b7c1
Emery <mcdonnea@purdue.edu>
2023-02-27 00:43:35 -0500

    Added note drawing

--- src/components/PianoRoll.tsx
+++ src/components/PianoRoll.tsx
@@ -1,38 +1,66 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useReducer, useRef} from 'react';
 
 export default function PianoRoll() {
+    const bgRef = useRef<HTMLCanvasElement | null>(null);
+    const fgRef = useRef<HTMLCanvasElement | null>(null);
+    
+
+    let rollWidth = 512;
+    let rollHeight = 384;
     let gridWidth = 16;
     let gridHeight = 16;
+    let computedStyle : CSSStyleDeclaration;
 
     useEffect(() => {
-        drawBG();
+        const bgCanvas = bgRef.current;
+        if (!bgCanvas) return; 
+        const bg = bgCanvas.getContext("2d");   
+        if (!bg) return;
+
+        const fgCanvas = fgRef.current;
+        if (!fgCanvas) return; 
+        const fg = fgCanvas.getContext("2d");   
+        if (!fg) return;
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        computedStyle = getComputedStyle(bgCanvas);
+
+        draw(bg, fg);
     });
 
-    function drawBG() {
-        const bgCanvas = document.getElementById("bgCanvas") as HTMLCanvasElement;
-        const bg = bgCanvas.getContext("2d");   
-        if (!bg) return; 
+    function draw(bg: CanvasRenderingContext2D, fg: CanvasRenderingContext2D) {
+        drawBG(bg);
+        drawNote(fg, 3, 3, computedStyle.getPropertyValue("--red"), computedStyle.getPropertyValue("--red-accent"));
+    }
 
-        let computedStyle = getComputedStyle(bgCanvas);
-        bg.imageSmoothingEnabled = false;
+    function drawBG(ctx: CanvasRenderingContext2D) {
+        ctx.imageSmoothingEnabled = false;
 
         // vertical grid lines
-        bg.fillStyle = computedStyle.getPropertyValue("--bg4");
-        for (let i = 0; i < bgCanvas.width / gridWidth; i++) {
-            bg.fillRect(gridWidth * (i + 1) - 1, 0, 1, bgCanvas.height);
+        ctx.fillStyle = computedStyle.getPropertyValue("--bg4");
+        for (let i = 0; i < rollWidth / gridWidth; i++) {
+            ctx.fillRect(gridWidth * (i + 1) - 1, 0, 1, rollHeight);
         }
 
         // horizontal grid lines
-        bg.fillStyle = computedStyle.getPropertyValue("--bg4");
-        for (let i = 0; i < bgCanvas.height / gridHeight; i++) {
-            bg.fillRect(0, gridHeight * (i+1) - 1, bgCanvas.width, 1)
+        ctx.fillStyle = computedStyle.getPropertyValue("--bg4");
+        for (let i = 0; i < rollHeight / gridHeight; i++) {
+            ctx.fillRect(0, gridHeight * (i+1) - 1, rollWidth, 1)
         }
+    }
+
+    function drawNote(ctx : CanvasRenderingContext2D, gridX : number, gridY : number, colorFill : string, colorOutline : string) {
+        ctx.fillStyle = colorFill;
+        ctx.fillRect(gridX * gridWidth, gridY * gridHeight, gridWidth - 1, gridHeight - 1);
+        ctx.strokeStyle = colorOutline;
+        ctx.lineWidth = 2;
+        ctx.strokeRect(gridX * gridWidth, gridY * gridHeight, gridWidth - 1, gridHeight - 1);
     }
 
     return(
         <div style={{position: "relative"}}>
-            <canvas id="bgCanvas" width="512" height="384" style={{position: "absolute", left: "0", top: "0", zIndex: "0", imageRendering: "pixelated"}} />
-            <canvas id="fgCanvas" width="512" height="384" style={{position: "absolute", left: "0", top: "0", zIndex: "1", imageRendering: "pixelated"}} />
+            <canvas ref={bgRef} width={rollWidth} height={rollHeight} style={{position: "absolute", left: "0", top: "0", zIndex: "0", imageRendering: "pixelated"}} />
+            <canvas ref={fgRef} width={rollWidth} height={rollHeight} style={{position: "absolute", left: "0", top: "0", zIndex: "1", imageRendering: "pixelated"}} />
         </div>
     )
 }

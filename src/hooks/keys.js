508f5c18a649a2c2e4148f802aeeabd9ccddbc2f
Mitchell Robert Bridwell <mbridwel@purdue.edu>
2023-02-26 22:21:03 -0500

    Added some more functions for keyboard shortcuts.

--- src/hooks/keys.js
+++ src/hooks/keys.js
@@ -1,9 +1,44 @@
-import {"useState, useEffect" from "react";
-
-function Keys() {
-    const pressLeft = usekeyPress("arrowLeft");
-    const moverightPress = useKeyPress("arrowRight");
-    const pittchUpPress = useKeyPress("arrowUp");
-    const pitchDownPress = useKeyPress("arrowDown");
-
-}// for each keypress call a function
+import React from 'react'
+import useKeyboardShortcut from 'use-keyboard-shortcut'
+   
+function keyMoveLeft() {
+    const { flushHeldKeys } = useKeyboardShortcut(
+  ["ArrowLeft"], setLocation(getLocation() - 1) ,{ overrideSystem: false, ignoreInputFields: false, repeatOnHold: false});
+    }
+     
+    function keyMoveRight() {
+        const { flushHeldKeys } = useKeyboardShortcut(
+      ["ArrowRight"], setLocation(getLocation() + 1) ,{ overrideSystem: false, ignoreInputFields: false, repeatOnHold: false});
+        }
+        
+    function keyIncreasePitch() {
+        const { flushHeldKeys } = useKeyboardShortcut(
+      ["ArrowLeft"], setPitch(getPitch()+1),{ overrideSystem: false, ignoreInputFields: false, repeatOnHold: false});
+        }
+        
+    function keyDecreasePitch() {
+        const { flushHeldKeys } = useKeyboardShortcut(
+      ["ArrowUp"], setLocation(getLocation() - 1) ,{ overrideSystem: false, ignoreInputFields: false, repeatOnHold: false});
+        }
+        
+    function keyIncreaseOctave() {
+        const { flushHeldKeys } = useKeyboardShortcut(
+      ["CTRL", "ArrowUp"], setPitch(getPitch()+12),{ overrideSystem: false, ignoreInputFields: false, repeatOnHold: false});
+        }
+        
+    function keyDecreaseOctave() {
+        const { flushHeldKeys } = useKeyboardShortcut(
+      ["CTRL", "ArrowDown"], setPitch(getPitch()-12),{ overrideSystem: false, ignoreInputFields: false, repeatOnHold: false});
+        }
+        
+    function keyCSixteenth() {
+        const { flushHeldKeys } = useKeyboardShortcut(
+      ["1"], setDuration(16)) ,{ overrideSystem: false, ignoreInputFields: false, repeatOnHold: false});
+        }
+        
+    function keyChangeEighth() {
+        const { flushHeldKeys } = useKeyboardShortcut(
+      ["2"], setDuration(8), { overrideSystem: false, ignoreInputFields: false, repeatOnHold: false});
+        }
+        
+        

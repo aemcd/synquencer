0632a67861e31582785ac6ff0a84c56a4418b368
Emery <mcdonnea@purdue.edu>
2023-03-01 00:04:40 -0500

    Refactored sequencer spaghetti code

--- src/components/PianoRoll.tsx
+++ src/components/PianoRoll.tsx
@@ -1,71 +1,73 @@
 import React, {useEffect, useRef, useReducer, MouseEvent} from 'react';
 
 export default function PianoRoll() {
-    const bgRef = useRef<HTMLCanvasElement | null>(null);
-    const fgRef = useRef<HTMLCanvasElement | null>(null);
-    let fg: CanvasRenderingContext2D;
-    let bg: CanvasRenderingContext2D;
     const sequenceMap = new Map<string, number>();
-    sequenceMap.set("3,5", 1);
-    sequenceMap.set("7,16", 2);
-    sequenceMap.set("9,7", 4);
 
     let rollWidth = 767;
     let rollHeight = 575;
     let gridWidth = 24;
     let gridHeight = 24;
+
+    const bgRef = useRef<HTMLCanvasElement | null>(null);
+    const fgRef = useRef<HTMLCanvasElement | null>(null);
+
     let computedStyle : CSSStyleDeclaration;
+    let bgCtx: CanvasRenderingContext2D | null;
+    let fgCtx: CanvasRenderingContext2D | null;
 
     useEffect(() => {
-        const bgCanvas = bgRef.current;
-        if (!bgCanvas) return;
-        const bg = bgCanvas.getContext("2d");
-        if (!bg) return;
+        computedStyle = getComputedStyle(document.body);
 
-        const fgCanvas = fgRef.current;
-        if (!fgCanvas) return;
-        fg = fgCanvas.getContext("2d")!;
-        if (!fg) return;
+        if (bgRef.current) {
+            bgCtx = bgRef.current.getContext("2d");
+        };
 
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        computedStyle = getComputedStyle(bgCanvas);
+        if (fgRef.current) {
+            fgCtx = fgRef.current.getContext("2d");
+        };
 
-        drawBG(bg);
-        drawFG(fg);
+        drawBG();
+        drawFG();
     });
 
-    function drawBG(ctx: CanvasRenderingContext2D) {
-        ctx.lineWidth = 2;
+    function drawBG() {
+        if (!bgCtx) return;
+
+        bgCtx.lineWidth = 2;
 
         // horizontal grid lines
-        ctx.fillStyle = computedStyle.getPropertyValue("--bg1");
+        bgCtx.fillStyle = computedStyle.getPropertyValue("--bg1");
         for (let i = 0; i < rollHeight / gridHeight; i++) {
-            ctx.fillRect(0, gridHeight * (i+1) - 1, rollWidth, 2)
+            bgCtx.fillRect(0, gridHeight * (i+1) - 1, rollWidth, 2)
         }
 
         // vertical grid lines
         for (let i = 0; i < rollWidth / gridWidth; i++) {
-            !((i + 1) % 4) ? ctx.fillStyle = computedStyle.getPropertyValue("--bg3") : ctx.fillStyle = computedStyle.getPropertyValue("--bg1");
-            ctx.fillRect(gridWidth * (i + 1) - 1, 0, 2, rollHeight);
+            !((i + 1) % 4) ? bgCtx.fillStyle = computedStyle.getPropertyValue("--bg3") : bgCtx.fillStyle = computedStyle.getPropertyValue("--bg1");
+            bgCtx.fillRect(gridWidth * (i + 1) - 1, 0, 2, rollHeight);
         }
     }
 
-    function drawFG(ctx: CanvasRenderingContext2D) {
-        ctx.clearRect(0, 0, rollWidth, rollHeight);
+    function drawFG() {
+        if (!fgCtx) return;
+
+        fgCtx.clearRect(0, 0, rollWidth, rollHeight);
 
         sequenceMap.forEach((value, key) => {
             let [x, y] = key.split(',').map(str => parseInt(str));
-            drawNote(ctx, x, y, value, computedStyle.getPropertyValue("--yellow"), computedStyle.getPropertyValue("--yellow-accent"));
+            drawNote(x, y, value, computedStyle.getPropertyValue("--yellow"), computedStyle.getPropertyValue("--yellow-accent"));
         });
     }
 
-    function drawNote(ctx : CanvasRenderingContext2D, gridX: number, gridY: number, length: number, colorFill: string, colorOutline: string) {
+    function drawNote(gridX: number, gridY: number, length: number, colorFill: string, colorOutline: string) {
         if (length < 0) return;
-        ctx.fillStyle = colorFill;
-        ctx.fillRect(gridX * gridWidth, gridY * gridHeight, gridWidth * length, gridHeight);
-        ctx.strokeStyle = colorOutline;
-        ctx.lineWidth = 2;
-        ctx.strokeRect(gridX * gridWidth, gridY * gridHeight, gridWidth * length, gridHeight);
+        if (!fgCtx) return;
+
+        fgCtx.fillStyle = colorFill;
+        fgCtx.fillRect(gridX * gridWidth, gridY * gridHeight, gridWidth * length, gridHeight);
+        fgCtx.strokeStyle = colorOutline;
+        fgCtx.lineWidth = 2;
+        fgCtx.strokeRect(gridX * gridWidth, gridY * gridHeight, gridWidth * length, gridHeight);
     }
 
     function handleClick(e: MouseEvent) {
@@ -84,7 +86,7 @@ export default function PianoRoll() {
             sequenceMap.set(`${gridX},${gridY}`, -1);
         }
 
-        drawFG(fg);
+        drawFG();
     }
 
     return(

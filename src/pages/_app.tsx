e589bfcad61768ce121c192d6623efed2ab99a22
mohini <mohroplax@gmail.com>
2023-03-25 23:38:35 -0400

    added several fluid/mongodb helper functions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,10 +11,11 @@ import {
 	PitchLocation,
 } from "@/server/types";
 import { SharedString } from "@fluidframework/sequence";
-import { IValueChanged, SharedMap } from "fluid-framework";
+import { IFluidContainer, IValueChanged, SharedMap } from "fluid-framework";
 import { TinyliciousClient } from "@fluidframework/tinylicious-client";
-import { GetNotes } from "../database/calls.js";
+import { AddNotes, AddSequence, GetNotes, GetSequence } from "../database/calls.js";
 import * as React from "react";
+import { type } from "os";
 
 const getFluidData = async () => {
 	const client: TinyliciousClient = new TinyliciousClient();
@@ -35,55 +36,66 @@ const getFluidData = async () => {
 		({ container } = await client.getContainer(containerId, schema));
 	}
 
-	return container.initialObjects;
+	return container;
 };
+
+let container: IFluidContainer;
 
 //METADATA CODE HERE ----------------------------------
 
-let localMetadata = new SequenceMetadata(); //local copy of properties for clients
+let localMetadata: SequenceMetadata; //local copy of properties for clients
 let localSequence: SharedMap;
 
-//TODO: finalize loading from mongodb
-//Serverside only
-/*
-export function loadMetadata(id: any): void {
-    metadataContainer.clear();
-    if (true) {//case where there is an existing sequence for the id
-        let sequenceArgs: any = Object.entries(localMetadata);
-        //TODO
-        //load metadata for sequence from mongodb and replace sequenceArgs
-        localMetadata = new SequenceMetadata(sequenceArgs);
+const loadSequence = async (id: string) => {//loads existing sequences and also initializes new ones
+	const databaseMetadata = await(GetSequence(id));
+	const databaseSequence = await(GetNotes(id));
+	let promisedMetadata: SequenceMetadata;
+	let promisedSequence: Note[];
+    if ((typeof databaseMetadata === 'undefined') || (typeof databaseMetadata === 'undefined')) {//case where we are creating new sequence
+        promisedMetadata = new SequenceMetadata();
+		promisedMetadata.id = id;
+		promisedSequence = [];
+		AddSequence(promisedMetadata);
     }
-    else {//case where we are creating new sequence
-
+    else {//case where there is an existing sequence for the id
+		promisedMetadata = databaseMetadata;
+		promisedSequence = databaseSequence as Note[];
     }
-}
-*/
-/*export function setMetadata(key: string, value: number | string) {
-    metadataContainer.set(key, value);
-}*/
-
-export function getMetadata(key: keyof SequenceMetadata) {
-	return localMetadata[key];
+	return {promisedMetadata, promisedSequence};
 }
 
-/*
-//serverside only
-export async function loadSequence(id: any) {
-    //TODO: Implement with database code
-    let instrumentList: Instrument[] = [new Instrument({channel: -1, name: "mayonnaise"}),];
-    for (let sequenceInstrument of instrumentList as Instrument[]) {
-        const newNoteList = await container.create(SharedMap);
-        sequenceContainer.set(sequenceInstrument.serialize(), newNoteList);
-    }
-    let noteList: {note: Note, instrument: Instrument}[] = [];
-    for (var notePattern of noteList as typeof noteList) {
-        addNote(notePattern.note, notePattern.instrument);
-    }
+const saveSequence = (arg: {metadata: SequenceMetadata, sequence: Note[]}) => {//might be a more efficient way to do this
+	AddSequence(arg.metadata);
+	AddNotes(arg.metadata.id, arg.sequence);
 }
-*/
+
+const sequenceDatabaseToSharedMap = async (list: Note[]) => {
+	let sequence = container.initialObjects.sequence as SharedMap; //i think this works with passing by reference?? needs to be tested
+	for (let note of list) {
+		const instrument = note.instrument.serialize();
+		if (!(Array.from(sequence.keys()).includes(instrument))) {//add new instrument submap to sequence map
+			const newNoteList = await container.create(SharedMap);
+			sequence.set(instrument, newNoteList);	
+		}
+		(sequence.get(instrument) as SharedMap).set(note.serialize(), note);
+	}
+	return sequence;
+}
+
+const sequenceSharedMapToDatabase = () => {
+	let list: Note[] = [];
+	let sequence = container.initialObjects.sequence as SharedMap; //i think this works with passing by reference?? needs to be tested
+	for (let value of Array.from(sequence.values())) {
+		const submap = value as SharedMap;
+		for (let note of Array.from(submap.values())) {
+			list.push(note as Note);
+		}
+	}
+	return list;
+}
 
 export default function App({ Component, pageProps }: AppProps) {
+	//TODO: fully implement fluid framework structures with React
 	// const [fluidMetadata, setMetadata] = React.useState<any | null>(null);
 	// const [fluidSequence, setSequence] = React.useState<any | null>(null);
 

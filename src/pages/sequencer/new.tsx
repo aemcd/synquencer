39ebbf0bba35ebc27d1eacf523a6806943142585
Zachary Heskett <zheskett@gmail.com>
2023-04-20 18:10:07 -0900

    test stuff

--- src/pages/sequencer/new.tsx
+++ src/pages/sequencer/new.tsx
@@ -62,6 +62,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 	metadata.set("bpm", newSeq.bpm);
 	metadata.set("numerator", newSeq.numerator);
 	metadata.set("denominator", newSeq.denominator);
+	container.dispose();
 
 	if (container != null) {
 		return {
